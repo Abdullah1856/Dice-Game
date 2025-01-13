4cf34b5cec8616d68abba35c7eaa91c1d094132e
Abdullah <68245138+Abdullah1856@users.noreply.github.com>
2025-01-12 20:52:07 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+//Selecting Score Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+// Rolling Dice Functionality
+btnRoll.addEventListener('click', function() {
+const dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+if(dice === 2) {
+   console.log('Dice is two');
+   diceEl.classList.remove('hidden');
+   document.querySelector('diceEl').textContent = 'a';
+}
+})
